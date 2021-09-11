e041ccad4c7dc4c2836ee7e0fe7933ba40d990e8
mononoken <ken.maeshima@pm.me>
2021-09-11 09:26:34 -0500

    Minor syntax cleanup.

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
-// Game will be played in the console only initially
-// First, program one singular round of RPS
+// Game will be played in the console only initially.
+// First, program one singular round of RPS.
 
-// Introduce game
+// Introduce game.
 const gameIntro = `This is a game of rock, paper, scissors.`;
 alert(gameIntro);
 
-// Create variables to represent selections: Rock, Paper, or Scissors
+// Create variables to represent selections: Rock, Paper, or Scissors.
 const rock = 'Rock';
 const paper = 'Paper';
 const scissors = 'Scissors';
 
-// Create variables representing the player and computer
+// Create variables representing the player and computer.
 player = 'Player';
 computer = 'Computer';
 
 // Get player selection
 let playerSelection = prompt("Pick 'Rock', 'Paper', or 'Scissors': ");
 
-// Convert player selection to case format to check selection criteria
+// Convert player selection to case format to check selection criteria.
 function convertSelection(selection) {
     convertedSelection = selection.slice(0,1).toUpperCase() + selection.slice(1,).toLowerCase();
     return convertedSelection;
@@ -25,26 +25,26 @@ function convertSelection(selection) {
 
 playerSelection = convertSelection(playerSelection);
 
-// Check player selection matches one of the three selections
+// Check player selection matches one of the three selections.
 function checkSelection(selection) {
     while ( selection !== rock && selection !== paper && selection !== scissors ) {
-        // Request player selection again until player selection matches criteria
+        // Request player selection again until player selection matches criteria.
         selection = prompt("Selection not recognized. Pick 'Rock', 'Paper', or 'Scissors': ");
         selection = convertSelection(selection);
     }
     return selection;
 }
 
-// Create variable for player selection
+// Create variable for player selection.
 playerSelection = checkSelection(playerSelection);
 
-// Get a random number from the Math functions between 0 and 3.
+// Setup a function to get a random number from the Math functions between 0 and 3.
 function getRandomNumber() {
     randomNumber = Math.floor(Math.random() * 3);
     return randomNumber;
 }
 
-// create function that picks computer selection randomly
+// Create function that picks computer selection randomly using the getRandomNumber function.
 function computerPlay() {
     let randomSelection;
     randomNumber = getRandomNumber();
@@ -61,7 +61,7 @@ function computerPlay() {
     return randomSelection;
 }
 
-// create variable for computer selection
+// Create variable for computer selection.
 let computerSelection = computerPlay();
 
 // Play one round of the game.
@@ -81,9 +81,9 @@ function playRound(playerSelection, computerSelection) {
     return result;
 }
 
+// Display selection of player and selection of computer.
 console.log(`Player picks ${playerSelection}.`)
 console.log(`Computer picks ${computerSelection}.`)
-console.log(playRound(playerSelection, computerSelection));
 
-// display selection of player and selection of computer
-// display who won the round
+// Display who won the round.
+console.log(playRound(playerSelection, computerSelection));
