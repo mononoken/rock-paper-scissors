fae005033fdb327a2f4406672b340cbf4fb02555
mononoken <ken.maeshima@pm.me>
2021-09-11 10:11:01 -0500

    Change player selection to fit into one function. Change play round so round prompts for playerSelection and computerSelection each round. Setup game function to play until one score reaches 3. Once 3 is reached, end game and report results.

--- scripts.js
+++ scripts.js
@@ -14,40 +14,45 @@ const scissors = 'Scissors';
 player = 'Player';
 computer = 'Computer';
 
-// Get player selection
-let playerSelection = prompt("Pick 'Rock', 'Paper', or 'Scissors': ");
+// Create function to get player selection including checks for proper input.
+function playerPlay() {
+    // Get player selection from input.
+    let playerSelection = prompt("Pick 'Rock', 'Paper', or 'Scissors': ");
 
-// Convert player selection to case format to check selection criteria.
-function convertSelection(selection) {
-    convertedSelection = selection.slice(0,1).toUpperCase() + selection.slice(1,).toLowerCase();
-    return convertedSelection;
-}
-
-playerSelection = convertSelection(playerSelection);
-
-// Check player selection matches one of the three selections.
-function checkSelection(selection) {
-    while ( selection !== rock && selection !== paper && selection !== scissors ) {
-        // Request player selection again until player selection matches criteria.
-        selection = prompt("Selection not recognized. Pick 'Rock', 'Paper', or 'Scissors': ");
-        selection = convertSelection(selection);
+    // Convert player selection to case format to check selection criteria.
+    function convertSelection(selection) {
+        convertedSelection = selection.slice(0,1).toUpperCase() + selection.slice(1,).toLowerCase();
+        return convertedSelection;
     }
-    return selection;
-}
 
-// Create variable for player selection.
-playerSelection = checkSelection(playerSelection);
+    playerSelection = convertSelection(playerSelection);
 
-// Setup a function to get a random number from the Math functions between 0 and 3.
-function getRandomNumber() {
-    randomNumber = Math.floor(Math.random() * 3);
-    return randomNumber;
+    // Check player selection matches one of the three selections.
+    function checkSelection(selection) {
+        while ( selection !== rock && selection !== paper && selection !== scissors ) {
+            // Request player selection again until player selection matches criteria.
+            selection = prompt("Selection not recognized. Pick 'Rock', 'Paper', or 'Scissors': ");
+            selection = convertSelection(selection);
+        }
+        return selection;
+    }
+
+    // Create variable for player selection.
+    playerSelection = checkSelection(playerSelection);
+
+    return playerSelection;
 }
 
 // Create function that picks computer selection randomly using the getRandomNumber function.
 function computerPlay() {
-    let randomSelection;
+    // Setup a function to get a random number from the Math functions between 0 and 3.
+    function getRandomNumber() {
+        randomNumber = Math.floor(Math.random() * 3);
+        return randomNumber;
+    }
     randomNumber = getRandomNumber();
+
+    let randomSelection;
     switch (randomNumber) {
         case 0:
             randomSelection = rock;
@@ -61,41 +66,40 @@ function computerPlay() {
     return randomSelection;
 }
 
-// Create variable for computer selection.
-let computerSelection = computerPlay();
-
 // Create variables representing player and computer scores.
 let playerScore = 0;
 let computerScore = 0;
 
 // Play one round of the game. Increment
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    // Create variable for computer selection.
+    let playerSelection = playerPlay();
+    let computerSelection = computerPlay();
     let result;
     // Check for winner based on if conditions and report the winner and increment winner score.
     if ( playerSelection === computerSelection ) {
         result = 'Draw.';
-        ++playerScore;
     } else if ( playerSelection === rock && computerSelection === scissors ) {
-        result = `${player} wins. ${playerSelection} beats ${computerSelection}.`;
+        result = `${player} wins round. ${playerSelection} beats ${computerSelection}.`;
         ++playerScore;
     } else if ( playerSelection === paper && computerSelection === rock ) {
-        result = `${player} wins. ${playerSelection} beats ${computerSelection}.`;
+        result = `${player} wins round. ${playerSelection} beats ${computerSelection}.`;
     } else if ( playerSelection === scissors && computerSelection === paper ) {
-        result = `${player} wins. ${playerSelection} beats ${computerSelection}.`;
+        result = `${player} wins round. ${playerSelection} beats ${computerSelection}.`;
         ++playerScore;
     } else {
-        result = `${computer} wins. ${computerSelection} beats ${playerSelection}.`;
+        result = `${computer} wins round. ${computerSelection} beats ${playerSelection}.`;
         ++computerScore;
     }
     return result;
 }
 
-// Display the results of the round.
-console.log(playRound(playerSelection, computerSelection));
-
-// Game setup to play 5 rounds.
+// Game setup to play 5 rounds maximum.
 function game() {
-    while ( playerScore !== 5 & computerScore !== 5 ) {
-        playRound(playerSelection, computerSelection);
+    while ( playerScore !== 3 && computerScore !== 3 ) {
+        console.log(playRound());
     }
+    ( playerScore === 3 ) ? console.log(`Player wins! Player: ${playerScore}. Computer: ${computerScore}.`) : console.log(`Computer wins. ${playerScore}. Computer: ${computerScore}.`);
 }
+
+game();
